147c171255b24d908971beca2b9ccd26819e1e92
ppp3ppj <ppp.3ppj@gmail.com>
2022-11-26 15:20:13 +0700

    fix bug

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -14,44 +15,53 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   // int _counter = 0;
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [IconButton(onPressed: () => Navigator.pushNamed(context, AppRoute.another), icon: Icon(Icons.skip_next))],
+        actions: [
+          IconButton(
+              onPressed: () => Navigator.pushNamed(context, AppRoute.another),
+              icon: Icon(Icons.skip_next))
+        ],
         title: Text(widget.title),
       ),
       body: BlocBuilder<CounterABloc, CounterAState>(
-  builder: (context, state) {
-    return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Counter A',
-            ),
+        builder: (context, state) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Counter A',
+                ),
                 Text(
                   // '${state.count}',
                   '${state.count}',
-              style: Theme.of(context).textTheme.headline4,
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
             ),
-          ],
-        ),
-      );
-  },
-),
+          );
+        },
+      ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: () => context.read<CounterABloc>().add(CounterAEventReset()),
+            heroTag: "A",
+            onPressed: () =>
+                context.read<CounterABloc>().add(CounterAEventReset()),
             tooltip: 'Reset',
             child: const Icon(Icons.restore),
           ),
-          const SizedBox(height: 10,),
+          const SizedBox(
+            height: 10,
+          ),
           FloatingActionButton(
-            onPressed: () => context.read<CounterABloc>().add(CounterAEventAdd()),
+            heroTag: "B",
+            onPressed: () =>
+                context.read<CounterABloc>().add(CounterAEventAdd()),
             tooltip: 'Increment',
             child: const Icon(Icons.add),
           ),
